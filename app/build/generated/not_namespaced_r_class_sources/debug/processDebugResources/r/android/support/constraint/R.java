fed17aaf2c8a1a5b70e6ce1be5427519ab61e811
DramaSetteR <blindgod@hotmail.com>
2018-12-09 23:14:45 +0400

    DoneSmth

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f070025;
-        public static final int end = 0x7f07003b;
-        public static final int gone = 0x7f070042;
-        public static final int invisible = 0x7f07004e;
-        public static final int left = 0x7f070050;
-        public static final int packed = 0x7f070074;
-        public static final int parent = 0x7f070075;
-        public static final int percent = 0x7f070077;
-        public static final int right = 0x7f07007b;
-        public static final int spread = 0x7f070094;
-        public static final int spread_inside = 0x7f070095;
-        public static final int start = 0x7f07009b;
-        public static final int top = 0x7f0700aa;
-        public static final int wrap = 0x7f0700b1;
+        public static final int bottom = 0x7f070027;
+        public static final int end = 0x7f070042;
+        public static final int gone = 0x7f07004c;
+        public static final int invisible = 0x7f070059;
+        public static final int left = 0x7f07005b;
+        public static final int packed = 0x7f070083;
+        public static final int parent = 0x7f070084;
+        public static final int percent = 0x7f070086;
+        public static final int right = 0x7f07008b;
+        public static final int spread = 0x7f0700a6;
+        public static final int spread_inside = 0x7f0700a7;
+        public static final int start = 0x7f0700af;
+        public static final int top = 0x7f0700c0;
+        public static final int wrap = 0x7f0700cc;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/net/walgo/mygame/Splash.java
+++ app/src/main/java/net/walgo/mygame/Splash.java
@@ -6,10 +6,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class Splash extends OBoard {
 
+
+
+
+private ImageView img1;
+    public void startFadeInAnimation() {
+        img1= findViewById(R.id.starimg);
+        Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+        img1.startAnimation(startAnimation);
+
+    }
 
 
     @SuppressLint("ClickableViewAccessibility")
@@ -17,29 +29,27 @@ public class Splash extends OBoard {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        ImageView image = findViewById(R.id.starimg);
+        startFadeInAnimation();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-                startActivity(new Intent(Splash.this, OBoard.class));
+                startActivity(new Intent(Splash.this, Menu.class));
                 overridePendingTransition(R.anim.fadein, R.anim.fadeout);
             }
-        }, 2000);
+        }, 1500);
 
-        image.setOnTouchListener(new View.OnTouchListener() {
+        img1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
-
+                        startActivity(new Intent(Splash.this, Menu.class));
+                        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
                         break;
                     case MotionEvent.ACTION_UP:
 
-                        startActivity(new Intent(Splash.this, OBoard.class));
-                        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+
                         break;
 
                     default:
@@ -51,6 +61,8 @@ public class Splash extends OBoard {
 
             }
         });
+
+
 
     }
 }
